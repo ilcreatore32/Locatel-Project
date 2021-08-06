7cdf055b85b33e75371512eaff14bf2c84875e0c
Weishler Berman <ilcreatore321@gmail.com>
2021-08-06 10:35:17 -0400

    Set navBar and loginModal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,95 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+/* Components */
+import Login from './components/Login/Login';
+import Window from './components/Modal/Modal';
+
+/* React- Bootstrap */
+import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+/* React-Router */
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import logo from './assets/images/logo.png';
 
 function App() {
+
   return (
-    <div>
-      <h1>Hola Mundo desde Locatel</h1>
-    </div>
-  )
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt="Logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {"   "}
+            Proyecto Locatel
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Router>
+        <div>
+          <Nav fill variant="tabs bg-dark" defaultActiveKey="/home">
+            <Nav.Item>
+              <NavLink
+                className="tab text-white nav-link"
+                activeClassName="tab-active"
+                to="/"
+                exact
+              >
+                Home
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink
+                className="tab text-white nav-link"
+                activeClassName="tab-active"
+                to="/Link-1"
+              >
+                Link 1
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink
+                className="tab text-white nav-link"
+                activeClassName="tab-active"
+                to="/Link-2"
+              >
+                Link 2
+              </NavLink>
+            </Nav.Item>
+          </Nav>
+          <Switch>
+            <Route path="/Link-2">
+              
+            </Route>
+            <Route path="/Link-1">
+            
+            </Route>
+            <Route path="/">
+              <Window title="Inicio de Sesión" button="Inicio de Sesión">
+                <Login />
+              </Window>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button  from 'react-bootstrap/Button';
+import { useState } from "react";
+
+function Window(props) {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+  return (
+    <>
+      <div className="modal-button">
+        <Button
+          variant="primary"
+          onClick={handleShow}
+          size={props.size}
+        >
+          {props.button}
+        </Button>
+      </div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title> {props.title} </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.children}</Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default Window
